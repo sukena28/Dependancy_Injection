9b819d2debe19f667ef75c0c5610c8a8ef97547e
sukenaSPL <62394795+sukena28@users.noreply.github.com>
2022-11-27 15:00:25 +0300

    Clean up

---
+++ Dependency_Injection/Models/DTOs/OrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Dependency_Injection.Models.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public OrderStatus Status { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Dependency_Injection/Program.cs
+++ Dependency_Injection/Program.cs
@@ -1,3 +1,4 @@
+using Dependency_Injection.Common.Configuration;
 using Dependency_Injection.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,20 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddAutoMapper(typeof(Program));
 //Register DI 
 builder.Services.AddServices();
-
-
+builder.Services.AddFilters();
+builder.Services.AddLogging();
 
 var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureExceptionHandler(logger);
 
 app.UseHttpsRedirection();
 
