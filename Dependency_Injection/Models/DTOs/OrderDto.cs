df5fb27b491e79e86d47349b49fd9b7e23ba65b3
sukenaSPL <62394795+sukena28@users.noreply.github.com>
2022-11-27 15:08:27 +0300

    Revert "Merge pull request #1 from sukena28/develop"
    This reverts commit 552831b34af3a3017bcebddcbf872cb82a9e9409, reversing
    changes made to 334513d1a02133f3abdec1fcf8be952762627e44.

--- Dependency_Injection/Models/DTOs/OrderDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dependency_Injection.Models.DTOs
-{
-    public class OrderDto
-    {
-        public int Id { get; set; }
-        public OrderStatus Status { get; set; }
-        public string Name { get; set; }
-    }
-}
